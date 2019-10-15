0edab6f48b2cb928cf135777ea6049d776a05aa2
Rizul <rizulgoyal23@gmail.com>
2019-10-15 18:16:04 -0400

    Initial Commit

---
+++ Group7_F2019_MAD3004_FP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group7_F2019_MAD3004_FP
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
